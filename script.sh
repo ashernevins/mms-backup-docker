de3a377ba6b8134329b0a6f44fa0c58e560f6aaa
Matej Kramny <matejkramny@gmail.com>
2014-06-29 06:54:59 +0100

    Fix config flag

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 
 sed -i "/mmsApiKey=/c\mmsApiKey=${MMS_KEY}" /etc/mongodb-mms/backup-agent.config
 
-/usr/bin/mongodb-mms-backup-agent -conf /etc/mongodb-mms/backup-agent.config 2>&1
+/usr/bin/mongodb-mms-backup-agent -c /etc/mongodb-mms/backup-agent.config 2>&1
